0c2bb0681b17edbdfbad6ac576c152f8c725f8d3
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2023-12-26 11:43:17 -0300

    feat: add function convertir

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import imagen0 from './imagenes/rey_atanagildo.png'
 import imagen1 from './imagenes/rey_sisebuto.png'
+import {useRef} from 'react'
 
 function App() {
+  const cambio = 23.16;
+  const refCaja = useRef(); //siempre que use una ref(referencia) tengo que usar el .current
   const incrementar = (e) =>{
     (e.target.innerHTML++);
+    
+  }
+  const convertir = () =>{
+    refCaja.current.innerHTML=Number(refCaja.current.innerHTML) * cambio;
   }
   return (
     <>
-      <div className="caja" onClick={incrementar}>1</div>
-      <button>Aceptar</button>
+      <div ref={refCaja} className="caja" onClick={incrementar}>1</div>
+      <button onClick={convertir}>Aceptar</button>
       <div>
         <img src={imagen0}></img>
       </div>
