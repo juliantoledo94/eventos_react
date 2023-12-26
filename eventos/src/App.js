aae73c296d7baf8f44f7b0bce246a7027d196f2f
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2023-12-26 17:50:32 -0300

    feat: add more functionality to funtions incrementar and cambioImagen

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -7,20 +7,48 @@ function App() {
   const cambio = 23.16;
   const refCaja = useRef(); //siempre que use una ref(referencia) tengo que usar el .current
   const incrementar = (e) =>{
-    (e.target.innerHTML++);
+    if(e.target.innerHTML>=9){
+      e.target.innerHTML=1;
+    }
+    else{
+      e.target.innerHTML++;
+    }
+    if(e.target.innerHTML>=8){
+      e.target.style.background="red"
+
+    }else{
+      e.target.style.background="none"
+    }
+    
     
   }
   const convertir = () =>{
     refCaja.current.innerHTML=Number(refCaja.current.innerHTML) * cambio;
   }
+
+  const cambioImagen = (e) =>{
+    if(e.target.src.includes("atanagildo")){
+      e.target.src=imagen1
+    }
+    else{
+      e.target.src=imagen0
+    }
+    //e.target.src=imagen1
+  }
+
+  const lectura = (e) =>{
+    //evento onChange hace que cuando escribo en el input me lo refleje en otro lugar ejemplo en la caja.
+    refCaja.current.innerHTML=e.target.value
+  }
+
   return (
     <>
       <div ref={refCaja} className="caja" onClick={incrementar}>1</div>
       <button onClick={convertir}>Aceptar</button>
       <div>
-        <img src={imagen0}></img>
+        <img onClick={cambioImagen} src={imagen0}></img>
       </div>
-      <input className="campo"></input>
+      <input onChange={lectura} className="campo"></input>
       
     </>
   );
